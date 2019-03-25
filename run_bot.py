2e4fb263c62118e7592a36675171f9254f1a01d8
NRGCODER <44064736+NRGCODER@users.noreply.github.com>
2019-03-25 09:36:57 +0300

    branch test merge

--- run_bot.py
+++ run_bot.py
@@ -5,7 +5,7 @@ import vk_api
 from config import *
 
 
-#testtest
+# testtest
 
 def write_msg(user_id, text):
     vk_bot.method('messages.send', {'user_id': user_id, 'message': text, 'random_id': random.randint(0, 1000)})
@@ -19,19 +19,30 @@ def write_msg_attach(user_id, text, att_url):
                    'random_id': random.randint(0, 1000)})
 
 
-def get_last_post(owner_id, count, offset, filter): # пишем новую функцию на основе метода wall.get
+def get_last_post(owner_id, count, offset, filter):  # пишем новую функцию на основе метода wall.get
     response = vk_bot_user.method('wall.get',
                                   {'owner_id': owner_id,
                                    'count': count,
                                    'offset': offset,  # offset = 1, в случае, если есть закрепленный пост
-                                   'filter': filter}) # т.к. он не самый свежий,
-                                                      # потому используем сдвиг на один пост
-    return response['items'][0]['id'] # все, что нам нужно от этого метода - id поста,
-                                      # return возвращает полученное значение
+                                   'filter': filter})  # т.к. он не самый свежий,
+    # потому используем сдвиг на один пост
+    return response['items'][0]['id']  # все, что нам нужно от этого метода - id поста,
+    # return возвращает полученное значение
 
 
-vk_bot_user = vk_api.VkApi(token=ACCOUNT_TOKEN) #wall.get только для юзеров,
-                                                # потому нужна отдельная авторизация
+def lp_update():
+    return json.loads((requests.get(
+        '{server}?need_pts={need_pts}&lp_version={lp_version}&access_token={token}&v={version}'.format(
+            server='https://api.vk.com/method/messages.getLongPollServer',
+            need_pts=1,
+            lp_version=3,
+            token=ACCESS_TOKEN,
+            version='5.80'))).text)['response']
+
+lp_server = lp_update()
+
+vk_bot_user = vk_api.VkApi(token=ACCOUNT_TOKEN)  # wall.get только для юзеров,
+# потому нужна отдельная авторизация
 
 vk_bot = vk_api.VkApi(token=ACCESS_TOKEN)
 long_poll = vk_bot.method('messages.getLongPollServer', {'need_pts': 1, 'lp_version': 3})
@@ -39,11 +50,7 @@ server, key, ts = long_poll['server'], long_poll['key'], long_poll['ts']
 print("готов к работе")
 
 while True:
-    long_poll = requests.get(
-        'https://{server}?act={act}&key={key}&ts={ts}&wait=500'.format(server=server,
-                                                                       act='a_check',
-                                                                       key=key,
-                                                                       ts=ts)).json()
+
     # print(long_poll)
     update = long_poll['updates']
     # print(update)
@@ -55,13 +62,13 @@ while True:
 
         if 'картинк' in update[0][6]:
             write_msg_attach(user_id,
-                             'вот тебе огненная картинка',
-                             'photo-171720905_456239025')
+                             'вот тебе огненная музыка',
+                             'audio2299551_456239087')
 
-        if 'красив' in update[0][6]: #ищет набор букв "красив" в сообщении
-            group_id = -35684707 #id групп всегда начинается с минуса
+        if 'красив' in update[0][6]:  # ищет набор букв "красив" в сообщении
+            group_id = -35684707  # id групп всегда начинается с минуса
             post_id = get_last_post(group_id, 1, 1, 'owner')
-            attach = 'wall' + str(group_id) + '_' + str(post_id) #формируем ссылку на пост
+            attach = 'wall' + str(group_id) + '_' + str(post_id)  # формируем ссылку на пост
             write_msg_attach(user_id, 'вот тебе красота', attach)
 
         print(str(user_name[0]['first_name']) + ' ' +
